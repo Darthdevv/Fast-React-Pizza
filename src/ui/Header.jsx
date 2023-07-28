6dd1917fd6d5402a94025ed6d6caf7da4b11118f
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-07-28 23:43:40 +0300

    🍕 header added

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Pizza Co.</Link>
+    </header>
+  )
+}
+
+export default Header
