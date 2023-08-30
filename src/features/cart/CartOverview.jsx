56e195380c857ed50449f519a47d31efa9fa00fc
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-08-31 02:05:25 +0300

    🍕 styling cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200 px-4 py-4 sm:px-6 uppercase">
+    <div className=" flex items-center justify-between bg-stone-800 text-stone-200 px-4 py-4 sm:px-6 uppercase text-sm    md:text-base">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
