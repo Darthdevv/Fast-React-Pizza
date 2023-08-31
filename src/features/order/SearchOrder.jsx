9f5ea81571a87d960a2877d909ce05af101bb8e7
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-08-31 17:56:17 +0300

    🍕 styling inputs

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,13 +1,26 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 
 function SearchOrder() {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery('');
+  }
+
   return (
-    <div>
-      <input placeholder="Search order #" value={query} onChange={e => setQuery(e.target.value)}
+    <form onChange={handleSubmit}>
+      <input placeholder="Search order #"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        className="w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 rounded-full px-4 py-2 text-sm placeholder:text-stone-400 "
       />
-    </div>
+    </form>
   )
 }
 
