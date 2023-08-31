e3f66b3a8eb808f39773a25904e795bd70dda878
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-08-31 03:01:21 +0300

    🍕 styling home

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 
 function Username() {
   return (
-    <div className="hidden text-sm font-semibold">Yousef</div>
+    <div className="hidden md:block text-sm font-semibold">Yousef</div>
   )
 }
 
