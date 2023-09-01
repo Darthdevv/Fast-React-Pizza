1aac37e64c78c84766aecc182f7107d0d67ea7c3
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-01 03:49:18 +0300

    🍕 custom Button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,10 @@
-function Button({children,disabled}) {
+import { Link } from "react-router-dom"
+
+function Button({ children, disabled, to }) {
+  const className = "bg-yellow-400  hover:bg-yellow-300 transition-colors duration-300 font-semibold uppercase text-stone-800 px-4 py-3 inline-block rounded-full tracking-wide focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
+  if (to) return <Link to={to} className={ className}>{ children}</Link>
   return (
-    <button className="bg-yellow-400  hover:bg-yellow-300 transition-colors duration-300 font-semibold uppercase text-stone-800 px-4 py-3 inline-block rounded-full tracking-wide focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4" disabled={disabled}>{ children}</button>
+    <button className={className} disabled={disabled}>{ children}</button>
   )
 }
 
