83ee2a465c73202991fd0e073d4c5e01879d5681
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-01 21:31:26 +0300

    🍕 Roboto font added

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,9 @@
 
 function Loader() {
   return (
-    <div className="loader"></div>
+    <div className="absolute bg-slate-200/20 inset-0 backdrop-blur-sm flex items-center justify-center">
+      <div className="loader"></div>
+    </div>
   )
 }
 
