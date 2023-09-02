85598630cffcda0448c894f9fc4c5db9d0b68978
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-02 07:02:10 +0300

    🍕 styling Cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -7,7 +7,7 @@ function MenuItem({ pizza }) {
   return (
     <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className={`h-24 ${soldOut? " opacity-70 grayscale" : ""}`} />
-      <div className="flex flex-col grow ">
+      <div className="flex flex-col grow pt-0.5">
         <p className=" font-medium">{name}</p>
         <p className=" font-sm capitalize italic text-stone-500">{ingredients.join(', ')}</p>
         <div className="mt-auto flex items-center justify-between">
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom"
 
 function Button({ children, disabled, to, type }) {
 
-  const className = "bg-yellow-400  hover:bg-yellow-300 transition-colors duration-300 font-semibold uppercase text-stone-800 px-4 py-3 inline-block rounded-full tracking-wide focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
+  const base = "bg-yellow-400  hover:bg-yellow-300 transition-colors duration-300 font-semibold uppercase text-stone-800 inline-block rounded-full tracking-wide focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    base: "bg-yellow-400  hover:bg-yellow-300 transition-colors duration-300 font-semibold uppercase text-stone-800 inline-block rounded-full tracking-wide focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary: 'border-2 border-stone-300 hover:bg-yellow-300 transition-colors duration-300 font-semibold uppercase text-stone-800 inline-block rounded-full tracking-wide focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 md:py-4'
   }
 
-  if (to) return <Link to={to} className={ className}>{ children}</Link>
+  if (to) return <Link to={to} className={styles[type]}>{ children}</Link>
   return (
-    <button className={className} disabled={disabled}>{ children}</button>
+    <button className={styles[type]} disabled={disabled}>{ children}</button>
   )
 }
 
