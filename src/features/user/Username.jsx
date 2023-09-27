db56ca39b778dec3a4fab9220b77db3049fc3095
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-27 09:20:30 +0300

    🍕 adding redux

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,9 @@
+import { useSelector } from "react-redux"
 
 function Username() {
+  const username = useSelector(state => state.user.username)
   return (
-    <div className="hidden md:block text-sm font-semibold">Yousef</div>
+    <div className="hidden md:block text-sm font-semibold">{ username}</div>
   )
 }
 
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,7 +22,7 @@
 import {createSlice} from '@reduxjs/toolkit'
 
 const initialState = {
-  username: '',
+  username: 'ara',
 }
 
 const userSlice = createSlice({
