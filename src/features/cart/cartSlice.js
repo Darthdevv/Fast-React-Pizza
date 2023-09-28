a0a90f2dc3239bbe03959545292c519ab71e5a17
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-28 14:56:31 +0300

    🍕 adding cart redux

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,13 +10,29 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    resetCart(state) {
-      state.cart = null;
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart.filter(item => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find(item => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find(item => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
     },
   },
 });
 
 
-export const { resetCart } = cartSlice.actions;
+export const { addItem,deleteItem,increaseItemQuantity,decreaseItemQuantity,clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
