e22ae4df2a7a3b3adf166808dc528044a6d48e9c
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-29 04:04:44 +0300

    🍕 adding cart redux

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
 
 function CartOverview() {
-  const totalCartQuantity = useSelector(state => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0));
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
 
-  const totalCartPrice = useSelector(state => state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0));
+  const totalCartPrice = useSelector(getTotalCartPrice);
   return (
     <div className=" flex items-center justify-between bg-stone-800 text-stone-200 px-4 py-4 sm:px-6 uppercase text-sm    md:text-base">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
