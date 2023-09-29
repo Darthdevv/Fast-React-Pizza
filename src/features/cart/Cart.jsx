5fb66027854e1fc5b71c4d103db85c5c488d62c2
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-29 04:34:12 +0300

    🍕 adding cart redux

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,33 +3,9 @@ import Button from '../../ui/Button';
 import CartItem from './CartItem';
 import { useSelector } from 'react-redux';
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function Cart() {
   const username = useSelector(state => state.user.username);
-  const cart = fakeCart;
+  const cart = useSelector(state => state.cart.cart);
 
   return (
     <div className='px-4 py-3'>
--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
-
   const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
   return (
     <div className=" flex items-center justify-between bg-stone-800 text-stone-200 px-4 py-4 sm:px-6 uppercase text-sm    md:text-base">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>${ totalCartPrice}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to='/cart'> Open cart &rarr;</Link>
     </div>
