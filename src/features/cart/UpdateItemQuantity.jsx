a168e83ee5e83bb024848be03dc81ece1517686c
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2023-09-30 15:17:16 +0300

    🍕 implementing inc/dec quantity done

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,14 @@
 import Button from "../../ui/Button"
+import { increaseItemQuantity } from "./cartSlice"
+import { decreaseItemQuantity } from "./cartSlice"
+import { useDispatch } from "react-redux"
 
-function UpdateItemQuantity() {
+function UpdateItemQuantity({pizzaId}) {
+  const dispatch = useDispatch();
   return (
-    <div>
-      <Button type='round'>-</Button>
-      <Button type='round'>+</Button>
+    <div className="flex gap-1 md:gap-3">
+      <Button onClick={()=> dispatch(decreaseItemQuantity(pizzaId))} type='round'>-</Button>
+      <Button onClick={()=> dispatch(increaseItemQuantity(pizzaId))} type='round'>+</Button>
     </div>
   )
 }
